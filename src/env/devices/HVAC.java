cc86e1e51a30c21b219d322e195d233204077735
Luis Gustavo Nardin <gnardin@emse.fr>
2023-07-02 12:42:23 +0000

    Set formatting

--- src/env/devices/HVAC.java
+++ src/env/devices/HVAC.java
@@ -7,39 +7,39 @@ import cartago.Artifact;
 import cartago.ObsProperty;
 
 public class HVAC extends Artifact {
-	private double temperature;
+  private double temperature;
 
-	void init(double temp) {
-		defineObsProperty("state", "off");
-		defineObsProperty("temperature", this.temperature);
-		log("Temperature: " + getObsProperty("temperature").doubleValue());
-	}
+  void init(double temp) {
+    defineObsProperty("state", "on");
+    defineObsProperty("temperature", this.temperature);
+    log("Temperature: " + getObsProperty("temperature").doubleValue());
+  }
 
-	@OPERATION
-	void turn_on() {
-		if (getObsProperty("state").stringValue().equals("off")) {
-			getObsProperty("state").updateValue("on");
-			this.execInternalOp("updateTemperatureProc", -1);
-			log("HVAC on");
-		}
-	}
+  @OPERATION
+  void turn_on() {
+    if (getObsProperty("state").stringValue().equals("off")) {
+      getObsProperty("state").updateValue("on");
+      this.execInternalOp("updateTemperatureProc", -1);
+      log("HVAC on");
+    }
+  }
 
-	@OPERATION
-	void turn_off() {
-		if (getObsProperty("state").stringValue().equals("on")) {
-			getObsProperty("state").updateValue("off");
-			log("HVAC off");
-		}
-	}
+  @OPERATION
+  void turn_off() {
+    if (getObsProperty("state").stringValue().equals("on")) {
+      getObsProperty("state").updateValue("off");
+      log("HVAC off");
+    }
+  }
 
-	@INTERNAL_OPERATION
-	void updateTemperatureProc(double step) {
-		ObsProperty temp = getObsProperty("temperature");
-		ObsProperty state = getObsProperty("state");
-		while (!state.stringValue().equals("off")) {
-			temp.updateValue(temp.doubleValue() + step);
-			log("Temperature: " + temp.doubleValue());
-			this.await_time(300);
-		}
-	}
+  @INTERNAL_OPERATION
+  void updateTemperatureProc(double step) {
+    ObsProperty temp = getObsProperty("temperature");
+    ObsProperty state = getObsProperty("state");
+    while (!state.stringValue().equals("off")) {
+      temp.updateValue(temp.doubleValue() + step);
+      log("Temperature: " + temp.doubleValue());
+      this.await_time(300);
+    }
+  }
 }
